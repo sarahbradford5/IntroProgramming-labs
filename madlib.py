400a150a8ecd7d42ce2d4f9a5b71ad4cd5610d43
Sarah <sarah.bradford1@marist.edu>
2020-02-06 17:25:46 -0500

    added madlib file

---
+++ madlib.py
@@ -0,0 +1,6 @@
+noun=input("enter a noun")
+verb=input("enter a verb")
+adjective=input("enter an adjective")
+place=input("enter a place")
+
+print("Once there was a", noun, "that", verb, "into a", adjective, place)
