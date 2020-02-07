43b268f6bc221b116469aae2da03fca354f53284
Sarah <sarah.bradford1@marist.edu>
2020-02-06 20:39:46 -0500

    added player customizations

--- game.py
+++ game.py
@@ -1,67 +1,69 @@
 def main():
     enter="press enter to continue"
-    name=input("enter a name")
-    =input("")
+    score=0
+    name=input("enter your name")
+    favoriteplayer=input("enter your favorite Red Sox player")
+    
 
-    print(name, "HELP! I am trying to go the Red Sox game at Fenway Park, but nothing is going my way!")
+    print(name, ", I am running late. Help me get to the Red Sox game tonight!")
 
     print() # Prints a blank line
     print("My dad just gave me his tickets to the baseball game tonight, but I am running late and I have to pick up my friend and then take the T into Boston")
-    print("Score:", 0)
+    print("I can't wait to see", favoriteplayer,)
+    print("Score:", score)
     input(enter)
     print()
 
-    print("Oh no! My car is almost out of gas. I have to stop and fill up your tank.")
-    print("Score:", 50)
+    print("Oh no", name, "! My car is almost out of gas. I have to stop and fill up your tank.")
+    print("Score:", score + 50)
     input(enter)
     print()
 
-    print("I finally get to my friends house, but she isn't ready to go yet. 10 minutes later she finally gets into the car and we head on our way.")
-    print("Score:", 100)
+    print("I get to my friends house, but she isn't ready to go yet. 10 minutes later she finally gets into the car and we head on our way.")
+    print("Score:", score + 100)
     input(enter)
     print()
 
     print("I get to the T station but, it already left without us.")
     print("We have to drive to another station to get to another T")
-    print("Score:", 150)
+    print("Score:", score + 150)
     input(enter)
     print()
 
     print("We finally get on the T and head into Boston")
-    print("Score:", 200)
+    print("Score:", score + 200)
     input(enter)
     print()
 
     print("We have arrived in Boston!")
-    print("Score:", 250)
+    print("Score:", score + 250)
     input(enter)
     print()
 
     print("Now we Uber to Fenway, and everything seems to be going our way.")
-    print("Score:", 300)
+    print("Score:", score + 300)
     input(enter)
     print()
 
     print("We finally get to Fenway and can't find our tickets in my backpack")
     print("Yes!! I finally found them in my pocket")
-    print("Score:", 350)
+    print("Score:", score + 350)
     input(enter)
     print()
 
     print("The game is about to start, we cut it close")
-    print("Score:", 400)
+    print("Score:", score + 400)
     input(enter)
     print()
 
-    print("My favorite player, Xander Bogaerts, hit a homerun right in the vacinity of my seat!")
+    print("My favorite player" , favoriteplayer, "hit a homerun right in the vacinity of my seat!")
     print("I caught the ball!!")
-    print("Score:", 450)
+    print("Score:", score + 450)
     input(enter)
     print()
     
     print("The Red Sox won the game! They are now doing post game interviews with some of our favorite players!")
-    print("I finally find them in my pocket")
-    print("Score:", 500)
+    print("Score:", score + 500)
     input(enter)
     print()
 
