762d9e23547808cd502e62d91f7f095e5e6650d0
Sarah <sarah.bradford1@marist.edu>
2020-02-06 20:01:22 -0500

    dried out code

--- game.py
+++ game.py
@@ -1,67 +1,71 @@
 def main():
-    print("HELP! I am trying to go the Red Sox game at Fenway Park, but nothing is going my way!")
+    enter="press enter to continue"
+    name=input("enter a name")
+    =input("")
+
+    print(name, "HELP! I am trying to go the Red Sox game at Fenway Park, but nothing is going my way!")
 
     print() # Prints a blank line
     print("My dad just gave me his tickets to the baseball game tonight, but I am running late and I have to pick up my friend and then take the T into Boston")
     print("Score:", 0)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
     print("Oh no! My car is almost out of gas. I have to stop and fill up your tank.")
     print("Score:", 50)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
     print("I finally get to my friends house, but she isn't ready to go yet. 10 minutes later she finally gets into the car and we head on our way.")
     print("Score:", 100)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
     print("I get to the T station but, it already left without us.")
     print("We have to drive to another station to get to another T")
     print("Score:", 150)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
     print("We finally get on the T and head into Boston")
     print("Score:", 200)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
     print("We have arrived in Boston!")
     print("Score:", 250)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
     print("Now we Uber to Fenway, and everything seems to be going our way.")
     print("Score:", 300)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
     print("We finally get to Fenway and can't find our tickets in my backpack")
     print("Yes!! I finally found them in my pocket")
     print("Score:", 350)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
     print("The game is about to start, we cut it close")
     print("Score:", 400)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
     print("My favorite player, Xander Bogaerts, hit a homerun right in the vacinity of my seat!")
     print("I caught the ball!!")
     print("Score:", 450)
-    input("Press Enter to continue...")
+    input(enter)
     print()
     
     print("The Red Sox won the game! They are now doing post game interviews with some of our favorite players!")
     print("I finally find them in my pocket")
     print("Score:", 500)
-    input("Press Enter to continue...")
+    input(enter)
     print()
 
-    print("You have won! Would you like to play again?")
+    print(name, "Congratulations, you have won! Thank you for your help! Would you like to play again?")
 
 
 main()
