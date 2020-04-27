f0f728f909e3678a6225854ad3852ba1c8532b1d
sarah <sarah.bradford1@marist.edu>
2020-04-27 15:57:50 -0400

    finalized credentials

--- credentials.py
+++ credentials.py
@@ -2,25 +2,22 @@
 # Lab #5 - Working with Strings and Functions
 # Author: Sarah Bradford
 # Created: 2020-02-24
-
 def user():
      first = input("Enter your first name: ")
      last = input("Enter your last name: ")
      return first,last
 
 def email():
-     x=user()
-     x = x[0] + "." + x[1]
-     return x 
+     first, last = user()
+     uname = str(first+"."+last)
+     return uname
 
-def password():
-     pswd = input("Create a new password: ")
-     return pswd
 
 def length():
-     x=password()
+     y = 0
+     pswd = input("Create a password: ")
      notCorrectLen = False
-     if len(x)<8:
+     if len(pswd)<8:
          notCorrectLen = True
      while notCorrectLen:
           print("password not long enough, must be 8 characters")
@@ -28,12 +25,22 @@ def length():
           if len(pswd)<8:
              notCorrectLen = True
           else:
-              notCorrectLen = False
-     print("strong password")
-     
+               notCorrectLen = False
+          while y==0:
+               if (pswd.islower()) == False and (pswd.isupper()) == False:
+                    print ("strong password")
+                    y=1
+               if (pswd.isupper())== True or (pswd.islower()) == True:
+                    y=0
+                    print("password must contain upper and lowercase letters")
+                    pswd=str(input("please enter a new password "))
+
+     return pswd
+               
+                       
 def main():
      uname=email()
-     length()
+     pswd = length()
      print("ok! Your new email address is", uname.lower(),"1@marist.edu")
      
 
