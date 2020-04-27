f0599a1acae08c45c798028fb950d0225a418d47
sarah <sarah.bradford1@marist.edu>
2020-04-26 20:35:09 -0400

    username

---
+++ credentials.py
@@ -0,0 +1,19 @@
+#CMPT 120 Intro to Programming
+# Lab #5 - Working with Strings and Functions
+# Author: Sarah Bradford
+# Created: 2020-02-24
+
+def main():
+     first = input("Enter your first name: ")
+     last = input("Enter your last name: ")
+     uname = first + last
+     pswd = input("Create a new password: ")
+
+#pswd need to be <8
+characters
+    while true: 
+          print("password is too short")
+          pswd = input("Create a new password: ")
+     print("strong password")
+     print("Account configured. Your new email address is",
+          uname + "@marist.edu")main()
